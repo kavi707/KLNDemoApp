da02fa66c53f793d0739f52157a0f2f565289867
Kavimal Wijewardana <kavimalw@gmail.com>
2025-03-20 22:53:24 -0400

    Created a new module call ui-dashboard

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,8 @@
 import java.net.URI
 
+include(":ui-dashboard")
+
+
 pluginManagement {
     resolutionStrategy {
         eachPlugin {
---
+++ ui-dashboard/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.kavi.droid.kln.demo.dashboard"
+    compileSdk = libs.versions.compilerSdkVersion.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.minSdkVersion.get().toInt()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        val javaVersion = JavaVersion.toVersion(libs.versions.jvmVersion.get().toInt())
+        sourceCompatibility = javaVersion
+        targetCompatibility = javaVersion
+    }
+    kotlinOptions {
+        jvmTarget = libs.versions.jvmVersion.get()
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.compose.material)
+    implementation(libs.androidx.navigation.compose)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
