5b73f4295aec22e38d5f8e24c78f6a0d703ecd29
Kavimal Wijewardana <kavimalw@gmail.com>
2025-03-22 11:33:22 -0400

    Create simple content in module A and module B UI

--- ui-module-a/src/main/kotlin/com/kavi/droid/kln/demo/module_a/ModuleAUI.kt
+++ ui-module-a/src/main/kotlin/com/kavi/droid/kln/demo/module_a/ModuleAUI.kt
@@ -1,12 +1,41 @@
 package com.kavi.droid.kln.demo.module_a
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ModuleAUI() {
-    Column {
-        Text(text = "Module A UI")
+    Scaffold { innerPadding ->
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Row (
+                Modifier
+                    .fillMaxWidth()
+                    .padding(top = 24.dp)
+            ) {
+                Text(
+                    modifier = Modifier
+                        .padding(8.dp),
+                    text = "Module A UI",
+                    style = MaterialTheme.typography.titleLarge
+                )
+            }
+        }
     }
 }
