7494843a76eff0bd84738255bd1e217660015a21
Kavimal Wijewardana <kavimalw@gmail.com>
2025-03-26 22:30:35 -0400

    Added images to Dept main screens in each module

--- ui-maths-dept/src/main/kotlin/com/kavi/droid/kln/demo/maths/MathsDeptUI.kt
+++ ui-maths-dept/src/main/kotlin/com/kavi/droid/kln/demo/maths/MathsDeptUI.kt
@@ -1,16 +1,27 @@
 package com.kavi.droid.kln.demo.maths
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -33,6 +44,21 @@ fun MathDeptUI() {
                         .padding(8.dp),
                     text = "Dept of Mathematics",
                     style = MaterialTheme.typography.titleLarge
+                )
+            }
+
+            Box(
+                modifier = Modifier
+                    .width(360.dp) // Adjust width as needed
+                    .height(240.dp) // Adjust height as needed
+                    .shadow(8.dp, shape = RoundedCornerShape(16.dp))
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(Color.White) // Optional to enhance shadow visibility
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.maths_image),
+                    contentDescription = "",
+                    modifier = Modifier.fillMaxSize()
                 )
             }
         }
--- ui-stat-dept/src/main/kotlin/com/kavi/droid/kln/demo/stat/StatDeptUI.kt
+++ ui-stat-dept/src/main/kotlin/com/kavi/droid/kln/demo/stat/StatDeptUI.kt
@@ -1,16 +1,29 @@
 package com.kavi.droid.kln.demo.stat
 
+import android.R.attr.contentDescription
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -35,6 +48,27 @@ fun StatDeptUI() {
                     style = MaterialTheme.typography.titleLarge
                 )
             }
+
+            Box(
+                modifier = Modifier
+                    .width(360.dp) // Adjust width as needed
+                    .height(240.dp) // Adjust height as needed
+                    .shadow(8.dp, shape = RoundedCornerShape(16.dp))
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(Color.White) // Optional to enhance shadow visibility
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.stat_image),
+                    contentDescription = "",
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
         }
     }
+}
+
+@Preview
+@Composable
+fun StatDeptUIPreview() {
+    StatDeptUI()
 }
