4c006041a8218b260f59f49ae01764aa8a630021
Kavimal Wijewardana <kavimalw@gmail.com>
2025-03-31 00:18:30 -0400

    Created Read-More/Read-Less UI functionality in stat & maths UI descriptions

--- ui-maths-dept/src/main/kotlin/com/kavi/droid/kln/demo/maths/ui/MathsDeptUI.kt
+++ ui-maths-dept/src/main/kotlin/com/kavi/droid/kln/demo/maths/ui/MathsDeptUI.kt
@@ -1,7 +1,12 @@
 package com.kavi.droid.kln.demo.maths.ui
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,6 +20,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -28,6 +37,9 @@ import com.kavi.droid.kln.demo.maths.R
 
 @Composable
 fun MathDeptUI() {
+
+    var isExpanded by remember { mutableStateOf(false) }
+
     Scaffold { innerPadding ->
         Column (
             modifier = Modifier
@@ -64,14 +76,34 @@ fun MathDeptUI() {
                 )
             }
 
-            Text(
-                modifier = Modifier
-                    .padding(12.dp),
-                text = stringResource(R.string.maths_dpt_desc),
-                style = TextStyle(
-                    fontSize = MaterialTheme.typography.bodySmall.fontSize
+            Column {
+                AnimatedContent(
+                    targetState = isExpanded,
+                    transitionSpec = {
+                        (fadeIn()).togetherWith(fadeOut())
+                    }
+                ) { isExpanded ->
+                    Text(
+                        modifier = Modifier
+                            .padding(12.dp),
+                        text = if (isExpanded) stringResource(R.string.maths_dpt_desc) else stringResource(R.string.maths_dpt_desc_short),
+                        style = TextStyle(
+                            fontSize = MaterialTheme.typography.bodySmall.fontSize
+                        )
+                    )
+                }
+
+                Text(
+                    modifier = Modifier
+                        .padding(start = 12.dp, top = 4.dp)
+                        .clickable { isExpanded = !isExpanded },
+                    text = if (isExpanded) stringResource(R.string.read_less) else stringResource(R.string.read_more),
+                    style = TextStyle(
+                        fontSize = MaterialTheme.typography.bodySmall.fontSize,
+                        color = MaterialTheme.colorScheme.primary
+                    )
                 )
-            )
+            }
         }
     }
 }
