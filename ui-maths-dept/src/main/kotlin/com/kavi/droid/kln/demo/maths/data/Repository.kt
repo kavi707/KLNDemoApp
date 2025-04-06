64c3f3ce272e35faab18a3351b20786cce223c9d
Kavimal Wijewardana <kavimalw@gmail.com>
2025-04-06 12:00:35 -0400

    Update the lecturers list in math-dept

--- ui-maths-dept/src/main/kotlin/com/kavi/droid/kln/demo/maths/data/Repository.kt
+++ ui-maths-dept/src/main/kotlin/com/kavi/droid/kln/demo/maths/data/Repository.kt
@@ -16,6 +16,46 @@ class Repository @Inject constructor() {
             name = "Dr. N.P.W.B.V.K. Senanayake",
             description = "Senior Lecturer (Grade I)\nPh.D., M.Sc.(Kyushu), B.Sc.(Kelaniya)",
             imageUrl = "https://science.kln.ac.lk/depts/maths/images/Staff/Academic/3.jpg"
+        ),
+        Lecturer(
+            name = "Mr. J. Munasinghe",
+            description = "Senior Lecturer  (Grade I)\nM.Sc.Nat(Kaiserslautern, Germany), B.Sc.(Kelaniya)",
+            imageUrl = "https://science.kln.ac.lk/depts/maths/images/Staff/Academic/6.jpg"
+        ),
+        Lecturer(
+            name = "Dr. (Mrs.) K.K.K.R. Perera",
+            description = "Senior Lecturer  (Grade I)\nPh.D, MSc.(Kyushu), M.Sc.(Colombo),B.Sc.(Kelaniya), Dip. NIBM, CBA",
+            imageUrl = "https://science.kln.ac.lk/depts/maths/images/Staff/Academic/1.jpg"
+        ),
+        Lecturer(
+            name = "Dr. (Mrs) G.S. Wijesiri",
+            description = "Senior Lecturer  (Grade I)\nPh.D, MA. (Oakland), B.Sc. (Kelaniya)",
+            imageUrl = "https://science.kln.ac.lk/depts/maths/images/Staff/Academic/7.jpg"
+        ),
+        Lecturer(
+            name = "Ms. N.A.S.N. Wimaladharma",
+            description = "Senior Lecturer  (Grade I)\nM.Phil (Kelaniya), B.Sc.(Kelaniya)",
+            imageUrl = "https://science.kln.ac.lk/depts/maths/images/Staff/Academic/8.jpg"
+        ),
+        Lecturer(
+            name = "Dr. (Mrs.) W.P.T. Hansameenu",
+            description = "Senior Lecturer  (Grade I)\nPh.D (Texas Tech, USA), M.Sc. (Texas Tech, USA) B.Sc.(Kelaniya)",
+            imageUrl = "https://science.kln.ac.lk/depts/maths/images/Staff/Academic/12.jpg"
+        ),
+        Lecturer(
+            name = "Dr. Upeksha Perera",
+            description = "Senior Lecturer  (Grade I) and Academic Advisor\nPhD (Potsdam, Germany), M.Sc.(Moratuwa), B.Sc.(Ruhuna) ,BCS , ACS",
+            imageUrl = "https://science.kln.ac.lk/depts/maths/images/Staff/Academic/11.jpg"
+        ),
+        Lecturer(
+            name = "Mr. K.D.W.J. Katugampola",
+            description = "Senior Lecturer  (Grade II)\nM.Phil (Kelaniya), B.Sc.(Kelaniya)",
+            imageUrl = "https://science.kln.ac.lk/depts/maths/images/Staff/Academic/9.jpg"
+        ),
+        Lecturer(
+            name = "Dr. (Mrs.) M. H. L.Weerasinghe",
+            description = "Senior Lecturer (Grade II)\nPh.D, M.Sc. (Missouri University of Science and Technology, USA) B.Sc. (Kelaniya)",
+            imageUrl = "https://science.kln.ac.lk/depts/maths/images/Staff/Academic/MHW.jpg"
         )
     )
 
--- ui-staff/src/main/kotlin/com/kavi/droid/kln/demo/staff/ui/StaffUI.kt
+++ ui-staff/src/main/kotlin/com/kavi/droid/kln/demo/staff/ui/StaffUI.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -39,7 +41,7 @@ class StaffUI @Inject constructor() {
     internal fun StaffListUI(staffData: StaffData?) {
         Column (
             modifier = Modifier
-                .height(600.dp)
+                .fillMaxSize()
                 .padding(12.dp)
                 .verticalScroll(rememberScrollState())
         ) {
