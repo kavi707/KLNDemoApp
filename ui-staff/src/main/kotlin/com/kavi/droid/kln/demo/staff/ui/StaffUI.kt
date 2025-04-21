9d30fdf403c9c62df91bf1081d41e04e38fc24a4
Kavimal Wijewardana <kavimalw@gmail.com>
2025-04-20 21:13:25 -0400

    Remove unused imports

--- ui-staff/src/main/kotlin/com/kavi/droid/kln/demo/staff/ui/StaffUI.kt
+++ ui-staff/src/main/kotlin/com/kavi/droid/kln/demo/staff/ui/StaffUI.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -17,7 +16,6 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
